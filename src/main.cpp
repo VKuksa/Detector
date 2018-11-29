2cf547ba69c61535dbfa9f335439e19ac3d01769
vkuksa <vkuksa@softserveinc.com>
2018-11-29 14:34:45 +0200

    Added boost via vcpkg and catch header to test folder

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/program_options.hpp>
+
+//Catch
+//https://blog.jetbrains.com/clion/2017/03/to-catch-a-clion/
+
+int main() {
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
